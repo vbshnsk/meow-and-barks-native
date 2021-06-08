252e2f7afb05dae99c323314ad4de164f9020281
vbshnsk <vlad.bushinskiy@gmail.com>
2021-06-08 23:01:47 +0300

    add map support without runtime permissions

--- meowsandbarks/app/src/main/java/mvvm/ui/MainActivity.kt
+++ meowsandbarks/app/src/main/java/mvvm/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package mvvm.ui
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
 import com.example.meowsandbarks.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nav_bar)
         supportActionBar?.hide()
+        findViewById<Button>(R.id.nav_bar_map).setOnClickListener {
+            findNavController(this, R.id.nav_empty)
+                    .navigate(R.id.action_global_mapFragment)
+        }
+        findViewById<Button>(R.id.nav_bar_home).setOnClickListener {
+            findNavController(this, R.id.nav_empty)
+                    .navigate(R.id.action_global_homeFragment)
+        }
+        findViewById<Button>(R.id.nav_bar_heart).setOnClickListener {
+            findNavController(this, R.id.nav_empty)
+                    .navigate(R.id.action_global_notificationsFragment)
+        }
+        findViewById<Button>(R.id.nav_bar_user).setOnClickListener {
+            findNavController(this, R.id.nav_empty)
+                    .navigate(R.id.action_global_profileFragment)
+        }
     }
 }
--- meowsandbarks/app/src/main/java/mvvm/ui/MapFragment.kt
+++ meowsandbarks/app/src/main/java/mvvm/ui/MapFragment.kt
@@ -1,6 +1,40 @@
 package mvvm.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.meowsandbarks.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_map, container, false)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        return v
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        //map.isMyLocationEnabled = true
+        map.addMarker(MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker"));
+    }
+
 }
